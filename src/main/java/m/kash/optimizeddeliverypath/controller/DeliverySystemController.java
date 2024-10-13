2a584986fd898c9742fa04686fbbcb0cf4869280
Kaushal <kaushalmarandi@icloud.com>
2024-10-13 16:36:26 +0530

    implemented dlivery system controller and api mappings

--- src/main/java/m/kash/optimizeddeliverypath/controller/DeliverySystemController.java
+++ src/main/java/m/kash/optimizeddeliverypath/controller/DeliverySystemController.java
@@ -1,13 +1,38 @@
 package m.kash.optimizeddeliverypath.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import m.kash.optimizeddeliverypath.dtos.RequestDto;
+import m.kash.optimizeddeliverypath.services.LoadBalancer;
+import m.kash.optimizeddeliverypath.services.OptimizedPathFinder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequestMapping("/shortest-delivery-path")
+@AllArgsConstructor
 public class DeliverySystemController {
 
+    private OptimizedPathFinder optimizedPathFinder;
+    private LoadBalancer loadBalancer;
 
+    @PostMapping("")
     public ResponseEntity<?> calculateShortestDeliveryPath(@RequestBody RequestDto requestDto){
-
+        try{
+            log.info("Request Body: {}", requestDto);
+            double response = optimizedPathFinder.minimumTimeCalculator(requestDto);
+            return new ResponseEntity<>(Map.of("minimun time", response), HttpStatus.OK);
+        } catch (Exception e){
+            log.error("Error occured in triggering api call: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+    @GetMapping("/server")
+    public void callServer(){
+        loadBalancer.loadBalancing();
     }
 }
